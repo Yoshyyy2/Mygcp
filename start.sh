994c8ca850cce46b61705594e5483f3f92db6148
Yoshyyy2 <igitkarl0@gmail.com>
2026-04-18 20:54:58 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Start keepalive
 /usr/local/bin/keepalive.sh &
-nginx &
+
+# Start nginx
+nginx -g 'daemon off;' &
+
+# Start Xray
 exec /usr/local/bin/xray run -config /etc/xray/config.json
