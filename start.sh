b9cce48f54ef9ff314bda438a770de90426888ef
Yoshyyy2 <igitkarl0@gmail.com>
2026-05-11 07:14:30 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Create nginx pid directory
-mkdir -p /run/nginx
+# Create required dirs
+mkdir -p /run/nginx /var/log/nginx
 
-# Start Xray in background (listens on 8081, 8082, 8083)
+# Test nginx config first
+nginx -t 2>&1
+
+# Start Xray in background
 /usr/local/bin/xray run -config /etc/xray/config.json &
+
+# Small delay to let xray start
+sleep 2
 
 # Start keepalive in background
 /usr/local/bin/keepalive.sh &
 
-# Start nginx in foreground (listens on 8080, proxies to Xray)
+# Start nginx in foreground
 exec nginx -g 'daemon off;'
